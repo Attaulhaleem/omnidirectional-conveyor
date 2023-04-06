01c3fe989b947c5ea779273627448149c21995d1
Atta ul Haleem <58297355+Attaulhaleem@users.noreply.github.com>
2023-04-07 01:06:36 +0500

    update improc.py

--- imgproc.py
+++ imgproc.py
@@ -1,8 +1,9 @@
 import numpy as np
 import cv2
 
-# Read image
-path = r"C:\Users\Lenovo\Pictures\Screenshots\Screenshot (4).png"
+# #Read image
+# path=r'C:\Users\Lenovo\Documents\FYP\box-1-biggest-contour.png'
+path = r"C:\Users\Lenovo\Pictures\Saved Pictures\pic2.jpg"
 img = cv2.imread(path)
 
 # Gaussian blur
@@ -11,39 +12,23 @@ imS = cv2.resize(blurred, (960, 540))
 cv2.imshow("output", imS)
 cv2.waitKey(0)
 
-# #Convert to graysscale
-# gray = cv2.cvtColor(blurred,cv2.COLOR_BGR2GRAY)
-# imS = cv2.resize(gray, (960, 540))                # Resize image
-# cv2.imshow("output", imS)
-# cv2.waitKey(0)
-
-
 # thresholding brown color
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-mask = cv2.inRange(hsv, (16, 69, 182), (26, 91, 230))
+# mask = cv2.inRange(hsv, (16,61,179), (22,100,236))
+mask = cv2.inRange(hsv, (0, 62, 0), (179, 255, 255))
 # kernel=np.ones((3,3),np.uint8)
 # erosion=cv2.erode(mask,kernel,iterations=1)
 imS = cv2.resize(mask, (960, 540))
 cv2.imshow("mask", imS)
 cv2.waitKey(0)
 
-
-# #Autocalculate the thresholding level
-# threshold = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
-
-
-# #Threshold
-# retval, bin = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
-# imS = cv2.resize(bin, (960, 540))                # Resize image
-# cv2.imshow("output", imS)
-# cv2.waitKey(0)
-
 # Find contours
 contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+maxarea = 100
 for cnt in contours:
     if cv2.contourArea(cnt) < 500:
         continue
-    if cv2.contourArea(cnt) > 55000:
+    if cv2.contourArea(cnt) > 38000:
         rect = cv2.minAreaRect(cnt)
         box = cv2.boxPoints(rect)
         box = np.int0(box)
@@ -63,47 +48,9 @@ for cnt in contours:
         box = cv2.boxPoints(rect)
         box = np.int0(box)
         cv2.drawContours(img, [box], 0, (0, 191, 255), 2)
-
-    # (x,y,w,h)=cv2.boundingRect(cnt)
-    # area=cv2.contourArea(cnt)
-    # print (area)
-
-    # if area>55000:
-    #     cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
-    # else:
-    #     cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+        # print(cv2.contourArea(cnt))
 
 
 imS = cv2.resize(img, (960, 540))
 cv2.imshow("output", imS)
 cv2.waitKey(0)
-
-# #Sort out the biggest contour (biggest area)
-# max_area = 0
-# max_index = -1
-# index = -1
-# for i in contours:
-#     area = cv2.contourArea(i)
-#     index=index+1
-#     if area > max_area :
-#         max_area = area
-#         max_index = index
-
-# #Draw the raw contours
-# contourpic=cv2.drawContours(imS, contours, max_index, (0, 255, 0), 3 )
-# cv2.imshow('contours', contourpic)
-# cv2.waitKey(0)
-# cv2.imwrite("box-1-biggest-contour.png", img)
-
-# #Draw a rotated rectangle of the minimum area enclosing our box (red)
-# cnt=contours[max_index]
-# rect = cv2.minAreaRect(cnt)
-# box = cv2.boxPoints(rect)
-# box = np.int0(box)
-# img = cv2.drawContours(img,[box],0,(0,0,255),2)
-
-# #Show original picture with contour
-# cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-# cv2.imshow('image', img)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
