f054be69ec3205ccd1aaf5b4434c866e7d63b156
Atta ul Haleem <58297355+Attaulhaleem@users.noreply.github.com>
2023-04-10 05:25:48 +0500

    added padding

--- hexagon.py
+++ hexagon.py
@@ -12,7 +12,15 @@ positions = list()
 count = 0
 
 # returns points for drawing hexagon, populates positions and coordinates
-def generate(length: int, rows: int, cols: int, flat_top: bool, odd_offset: bool):
+def generate(
+    length: int,
+    rows: int,
+    cols: int,
+    flat_top: bool,
+    odd_offset: bool,
+    pad_x: int,
+    pad_y: int,
+):
     global coordinates, positions, FLAT_TOP, ODD_OFFSET, count
     coordinates.clear()
     positions.clear()
@@ -26,8 +34,8 @@ def generate(length: int, rows: int, cols: int, flat_top: bool, odd_offset: bool
             for row in range(rows):
                 if not col % 2 == odd_offset and row == 0 and rows > 1:
                     continue
-                X = col * 1.5 * L
-                Y = row * SQRT_3 * L
+                X = pad_x + col * 1.5 * L
+                Y = pad_y + row * SQRT_3 * L
                 Y += 0.5 * SQRT_3 * L if col % 2 == odd_offset else 0
                 drawingPoints.append(
                     [
@@ -50,9 +58,9 @@ def generate(length: int, rows: int, cols: int, flat_top: bool, odd_offset: bool
             for col in range(cols):
                 if not row % 2 == odd_offset and col == 0 and cols > 1:
                     continue
-                X = col * SQRT_3 * L
+                X = pad_x + col * SQRT_3 * L
                 X += 0.5 * SQRT_3 * L if row % 2 == odd_offset else 0
-                Y = row * 1.5 * L
+                Y = pad_y + row * 1.5 * L
                 drawingPoints.append(
                     [
                         (X + 0.5 * SQRT_3 * L, Y),
