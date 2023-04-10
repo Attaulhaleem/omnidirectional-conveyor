7c65ee5d1e3374e1807d1e22cf25d66c33eca7bc
Atta ul Haleem <58297355+Attaulhaleem@users.noreply.github.com>
2023-04-10 09:21:24 +0500

    update .gitignore

--- videofeed.py
+++ videofeed.py
@@ -1,30 +1,25 @@
 import cv2
 from tkinter import *
-from tkinter import ttk
 from PIL import Image, ImageTk
 
 
-class VideoFeed:
-    def __init__(self):
+class LabelVideoFeed:
+    def __init__(self, label, width, height, fps):
         self.vs = cv2.VideoCapture(0)
         self.current_image = None
-        self.root = Tk()
-        self.frame = ttk.Frame(self.root)
-        self.label = ttk.Label(self.frame)
-        self.frame.grid(column=0, row=0)
-        self.label.grid(column=0, row=0)
+        self.label = label
+        self.width = width
+        self.height = height
+        self.fps = fps
         self.video_loop()
-        self.root.mainloop()
 
     def video_loop(self):
         ok, frame = self.vs.read()
         if ok:
             cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
             self.current_image = Image.fromarray(cv2image)
+            self.current_image = self.current_image.resize((self.width, self.height))
             imgtk = ImageTk.PhotoImage(image=self.current_image)
             self.label.configure(image=imgtk)
             self.label.image = imgtk
-        self.root.after(30, self.video_loop)
-
-
-VideoFeed()
+        self.label.after(int(1000 / self.fps), self.video_loop)
