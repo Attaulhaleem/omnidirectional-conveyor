a2f527e2abfec52c2e67c2bb61f4ae6c2cedc0e5
Atta ul Haleem <58297355+Attaulhaleem@users.noreply.github.com>
2023-04-10 07:10:46 +0500

    updated

--- gui_tk.py
+++ gui_tk.py
@@ -2,6 +2,11 @@ from tkinter import *
 from tkinter import ttk
 from PIL import Image, ImageTk
 import hexagon
+import cv2
+
+# FONTS
+HEADING_FONT = ("TkHeadingFont", 14)
+BODY_FONT = ("TkTextFont", 10)
 
 # top level window
 root = Tk()
@@ -9,16 +14,15 @@ root.state("zoomed")
 
 # main window
 main_frame = ttk.Frame(root, padding=10)
-
 # define style for Frame template
 normal_frame_style = ttk.Style()
-normal_frame_style.configure("Normal.TFrame", relief="ridge", borderwidth=10, padding=5)
+normal_frame_style.configure("Normal.TFrame", padding=5)
 
 # frame for title and subtitle
 title_frame = ttk.Frame(main_frame, style="Normal.TFrame")
 
 # load and resize title image
-title_image = Image.open("app/assets/hexagons1.png")
+title_image = Image.open("app/assets/hex-box-blue.png")
 title_image = title_image.resize((120, 120))
 title_image = ImageTk.PhotoImage(title_image)
 # add image to label
@@ -41,40 +45,64 @@ subtitle_label = ttk.Label(
 )
 
 # frame for hexagons canvas and statistics
-display_frame = ttk.Frame(main_frame, style="Normal.TFrame")
+display_frame = ttk.Frame(main_frame, style="Normal.TFrame", relief="raised")
 
 # hexagon drawing
-hexagons_canvas = Canvas(display_frame)
+hexagons_canvas = Canvas(
+    display_frame, highlightthickness=1, highlightbackground="black"
+)
 
 grid = hexagon.generate(50, 3, 4, False, True, 10, 10)
-for coords in grid:
+for i in range(len(grid)):
     hexagons_canvas.create_polygon(
-        coords, fill="#00a1af", outline="#000000", activefill="#59a310"
+        grid[i], fill="#00a1af", outline="black", activefill="#59a310"
+    )
+    hexagons_canvas.create_text(
+        hexagon.positions[i][0], hexagon.positions[i][1], text=str(i + 1)
     )
 for widget in hexagons_canvas.winfo_children():
     widget.grid_configure(padx=10, pady=10)
 
-display_label = ttk.Label(
+# load and resize config image
+config_image = Image.open("app/assets/hexagons1.png")
+config_image = config_image.resize((50, 50))
+config_image = ImageTk.PhotoImage(config_image)
+
+display_title_label = ttk.Label(
     display_frame,
-    text="Rows:\t\t3\n\nColumns:\t\t4\n\nOrientation:\tPointy Top\n\nOffset:\t\tOdd",
-    font=("TkHeadingFont", 12),
+    text="Configuration",
+    font=HEADING_FONT,
+    compound="left",
+    image=config_image,
     anchor="center",
 )
 
-feed_frame = ttk.Frame()
+display_label = ttk.Label(
+    display_frame,
+    text="Rows:\t\t3\n\nColumns:\t\t4\n\nOrientation:\tPointy Top\n\nOffset:\t\tOdd",
+    font=BODY_FONT,
+    anchor="center",
+    relief="ridge",
+    padding=10,
+)
+
+feed_canvas = Canvas(display_frame, highlightthickness=1, highlightcolor="black")
+feed_canvas.create_rectangle(50, 50, 100, 100)
+
 # align objects to grid
-main_frame.grid(
-    column=0, row=0, sticky=(N, S, E, W)
-)  # column=0, row=0, sticky=(N, S, E, W))
+main_frame.grid(column=0, row=0, sticky=(N, S, E, W))
 # title
-title_frame.grid(column=0, row=0, rowspan=2, columnspan=2, sticky=(N, S, E, W))
+title_frame.grid(column=0, row=0, sticky=(N, S, E, W))
 title_image_label.grid(column=0, row=0, rowspan=2, sticky=(N, S, E, W))
 title_text_label.grid(column=1, row=0, sticky=(N, S, E, W))
 subtitle_label.grid(column=1, row=1, sticky=(N, S, E, W))
 # display
-display_frame.grid(column=0, row=2, sticky=(N, S, E, W))
-hexagons_canvas.grid(column=0, row=0, padx=20, pady=20, sticky=(N, S, E, W))
-display_label.grid(column=1, row=0, sticky=(N, S, E, W))
+display_frame.grid(column=0, row=1, sticky=(N, S, E, W))
+hexagons_canvas.grid(column=0, row=0, rowspan=2, padx=20, pady=20, sticky=(N, S, E, W))
+display_title_label.grid(column=1, row=0, sticky=(N, S, E, W))
+display_label.grid(column=1, row=1, sticky=(N, S, E, W))
+feed_canvas.grid(column=2, row=0, rowspan=2, sticky=(N, S, E, W))
+# control
 
 # set resizing properties
 root.columnconfigure(0, weight=1)
@@ -82,7 +110,7 @@ root.rowconfigure(0, weight=1)
 main_frame.columnconfigure(0, weight=1)
 title_frame.columnconfigure(1, weight=1)
 display_frame.columnconfigure(0, weight=1)
-hexagons_canvas.columnconfigure(0, weight=1)
-hexagons_canvas.rowconfigure(0, weight=1)
+display_frame.columnconfigure(1, weight=1)
+display_frame.columnconfigure(2, weight=1)
 
 root.mainloop()
