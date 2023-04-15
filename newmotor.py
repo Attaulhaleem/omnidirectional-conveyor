420866fcf2aa0e2db1ca1891654c61c06aa043d8
Atta ul Haleem <58297355+Attaulhaleem@users.noreply.github.com>
2023-04-15 07:02:24 +0500

    add newmotor.py

---
+++ newmotor.py
@@ -0,0 +1,36 @@
+import RPi.GPIO as GPIO
+import time
+
+# define PINs according to cabling
+dataPIN = 11
+latchPIN = 13
+clockPIN = 15
+
+# set pins to putput
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup((dataPIN, latchPIN, clockPIN), GPIO.OUT)
+
+# define shift register update function
+def shift_update(input, data, clock, latch):
+    # put latch down to start data sending
+    GPIO.output(clock, 0)
+    GPIO.output(latch, 0)
+    GPIO.output(clock, 1)
+
+    # load data in reverse order
+    for i in range(7, -1, -1):
+        GPIO.output(clock, 0)
+        GPIO.output(data, int(input[i]))
+        GPIO.output(clock, 1)
+
+    # put latch up to store data on register
+    GPIO.output(clock, 0)
+    GPIO.output(latch, 1)
+    GPIO.output(clock, 1)
+
+
+shift_update(int("0b01000110"), dataPIN, clockPIN, latchPIN)
+time.sleep(1000)
+
+# PINs final cleaning
+GPIO.cleanup()
