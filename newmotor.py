dcbac49d6d5800ab8a719be3f605bd37a44be1a2
Atta ul Haleem <58297355+Attaulhaleem@users.noreply.github.com>
2023-04-15 10:38:19 +0500

    update

--- newmotor.py
+++ newmotor.py
@@ -29,12 +29,6 @@ def shift_update(input, data, clock, latch):
     GPIO.output(clock, 1)
 
 
-for i in range(8):
-    data = [0 for _ in range(8)]
-    data[i] = 1
-    shift_update(data, dataPIN, clockPIN, latchPIN)
-    time.sleep(5)
-
 shift_update([0, 1, 1, 0, 0, 0, 0, 1], dataPIN, clockPIN, latchPIN)
 shift_update([0, 1, 1, 0, 0, 0, 0, 1], dataPIN, clockPIN, latchPIN)
 shift_update([0, 1, 1, 0, 0, 0, 0, 1], dataPIN, clockPIN, latchPIN)
