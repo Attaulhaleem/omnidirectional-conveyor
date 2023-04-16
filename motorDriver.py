8b9c8f53e9ac7350f303e2b0d3b7ab98b2b93e7d
Atta ul Haleem <58297355+Attaulhaleem@users.noreply.github.com>
2023-04-17 03:04:56 +0500

    testing

--- motorDriver.py
+++ motorDriver.py
@@ -4,7 +4,7 @@ import motor
 
 motor.setup()
 
-data_list = motor.getChainedBinaryList(motor.SampleStates)
+data_list = motor.getChainedBinaryList([motor.CLOCKWISE for _ in range(4)])
 motor.shiftOut(data_list)
 time.sleep(100)
 
