9f40279166c2a72d103e4686418fc1d6c60ffa77
Atta ul Haleem <58297355+Attaulhaleem@users.noreply.github.com>
2023-04-16 23:47:25 +0500

    added pulse functions

--- newmotor.py
+++ newmotor.py
@@ -6,41 +6,65 @@ dataPIN = 11
 latchPIN = 13
 clockPIN = 15
 
+# delay for level change (seconds)
+delay = 0.01
+
 # set pins to putput
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup((dataPIN, latchPIN, clockPIN), GPIO.OUT)
+GPIO.output((dataPIN, latchPIN, clockPIN), GPIO.LOW)
+
+
+def pulse_clock():
+    GPIO.output(clockPIN, GPIO.LOW)
+    time.sleep(delay)
+    GPIO.output(clockPIN, GPIO.HIGH)
+    time.sleep(delay)
+    GPIO.output(clockPIN, GPIO.LOW)
+    time.sleep(delay)
+
+
+def pulse_latch():
+    GPIO.output(latchPIN, GPIO.LOW)
+    time.sleep(delay)
+    GPIO.output(latchPIN, GPIO.HIGH)
+    time.sleep(delay)
+    GPIO.output(latchPIN, GPIO.LOW)
+    time.sleep(delay)
+
 
 # define shift register update function
-def shift_update(input, data, clock, latch):
+def shift_update(input):
     # put latch down to start data sending
     # GPIO.output(clock, 0)
-    GPIO.output(latch, 0)
-    time.sleep(0.01)
+    # GPIO.output(latchPIN, 0)
+    # time.sleep(0.01)
     # GPIO.output(clock, 1)
 
     # load data in reverse order
     for i in range(7, -1, -1):
-        GPIO.output(clock, 0)
-        time.sleep(0.01)
-        GPIO.output(data, int(input[i]))
-        time.sleep(0.01)
-        GPIO.output(clock, 1)
-        time.sleep(0.01)
+        # GPIO.output(clock, 0)
+        # time.sleep(0.01)
+        GPIO.output(dataPIN, int(input[i]))
+        pulse_clock()
+        # time.sleep(0.01)
+        # GPIO.output(clock, 1)
+        # time.sleep(0.01)
 
 
-def shift_latch(clock, latch):
-    # put latch up to store data on register
-    # GPIO.output(clock, 0)
-    GPIO.output(latch, 1)
-    time.sleep(0.1)
-    # GPIO.output(clock, 1)
+# def shift_latch(clock, latch):
+#     # put latch up to store data on register
+#     # GPIO.output(clock, 0)
+#     GPIO.output(latch, 1)
+#     time.sleep(0.1)
+#     # GPIO.output(clock, 1)
 
 
 shift_update([0, 0, 1, 0, 0, 1, 0, 0], dataPIN, clockPIN, latchPIN)
 shift_update([0, 0, 1, 0, 0, 1, 0, 0], dataPIN, clockPIN, latchPIN)
 shift_update([0, 0, 1, 0, 0, 1, 0, 0], dataPIN, clockPIN, latchPIN)
 shift_update([0, 0, 1, 0, 0, 1, 0, 0], dataPIN, clockPIN, latchPIN)
-shift_latch(clockPIN, latchPIN)
+pulse_latch()
 time.sleep(1000)
 
 # PINs final cleaning
