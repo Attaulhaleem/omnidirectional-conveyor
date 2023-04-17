489961f576094e6be537e3c0c2212dc161d19c63
Atta ul Haleem <58297355+Attaulhaleem@users.noreply.github.com>
2023-04-17 06:56:30 +0500

    updated keyboard interrupt

--- motorDriver.py
+++ motorDriver.py
@@ -7,9 +7,10 @@ motor.setup()
 data_list = motor.getChainedBinaryList([motor.CLOCKWISE for _ in range(4)])
 motor.shiftOut(data_list)
 
-try:
-    time.sleep(1)
-except KeyboardInterrupt:
-    print("Manually exiting program!")
-    GPIO.cleanup()
-    raise SystemExit
+while True:
+    try:
+        time.sleep(1)
+    except KeyboardInterrupt:
+        print("Manually exiting program!")
+        GPIO.cleanup()
+        raise SystemExit
