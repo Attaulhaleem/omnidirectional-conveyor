a4d0d4d2f961dd6d29f4d06a08d54b5852a0f7f2
Atta ul Haleem <58297355+Attaulhaleem@users.noreply.github.com>
2023-04-24 00:17:27 +0500

    changed STATES and ACTIONS to dicts

--- dev/module.py
+++ dev/module.py
@@ -1,38 +1,16 @@
 from motor import *
 
-# ACTIONS = {
-#     "idle": (RELEASE, RELEASE, RELEASE),
-#     "up_right": (RELEASE, FORWARD, BACKWARD),
-#     "right": (FORWARD, RELEASE, BACKWARD),
-#     "down_right": (FORWARD, BACKWARD, RELEASE),
-#     "down_left": (RELEASE, BACKWARD, FORWARD),
-#     "left": (BACKWARD, RELEASE, FORWARD),
-#     "up_left": (BACKWARD, FORWARD, RELEASE),
-#     "clockwise": (FORWARD, FORWARD, FORWARD),
-#     "anti_clockwise": (BACKWARD, BACKWARD, BACKWARD),
-# }
-
-IDLE = (RELEASE, RELEASE, RELEASE)
-UP_RIGHT = (RELEASE, FORWARD, BACKWARD)
-RIGHT = (FORWARD, RELEASE, BACKWARD)
-DOWN_RIGHT = (FORWARD, BACKWARD, RELEASE)
-DOWN_LEFT = (RELEASE, BACKWARD, FORWARD)
-LEFT = (BACKWARD, RELEASE, FORWARD)
-UP_LEFT = (BACKWARD, FORWARD, RELEASE)
-CLOCKWISE = (FORWARD, FORWARD, FORWARD)
-ANTI_CLOCKWISE = (BACKWARD, BACKWARD, BACKWARD)
-
-VALID_ACTIONS = (
-    IDLE,
-    UP_RIGHT,
-    RIGHT,
-    DOWN_RIGHT,
-    DOWN_LEFT,
-    LEFT,
-    UP_LEFT,
-    CLOCKWISE,
-    ANTI_CLOCKWISE,
-)
+ACTIONS = {
+    "idle": (STATES["release"], STATES["release"], STATES["release"]),
+    "up_right": (STATES["release"], STATES["forward"], STATES["backward"]),
+    "right": (STATES["forward"], STATES["release"], STATES["backward"]),
+    "down_right": (STATES["forward"], STATES["backward"], STATES["release"]),
+    "down_left": (STATES["release"], STATES["backward"], STATES["forward"]),
+    "left": (STATES["backward"], STATES["release"], STATES["forward"]),
+    "up_left": (STATES["backward"], STATES["forward"], STATES["release"]),
+    "clockwise": (STATES["forward"], STATES["forward"], STATES["forward"]),
+    "anti_clockwise": (STATES["backward"], STATES["backward"], STATES["backward"]),
+}
 
 
 class Module:
@@ -42,10 +20,10 @@ class Module:
     # output format [3A, 4B, 3B, 2B, 1B, 1A, 2A, 4A]
     PIN_CONFIG = ((5, 4), (6, 3), (0, 2))  # motor pins on shift register output
 
-    def __init__(self, position, action=IDLE):
+    def __init__(self, position, action=ACTIONS["idle"]):
         """Initialize a module (containing 3 motors).
         position: module center position in pi camera frame
-        action: any action from VALID_ACTIONS"""
+        action: any action from ACTIONS"""
         self.position = position
         self.motors = [
             Motor(self.PIN_CONFIG[i], self.get_motor_positions(position)[i])
@@ -60,8 +38,8 @@ class Module:
 
     def set_action(self, action):
         """Set module action (and corresponding motor states).
-        action: tuple of three motor states"""
-        if action not in VALID_ACTIONS:
+        action: any action in ACTIONS"""
+        if action not in ACTIONS.values():
             raise Exception("Invalid module action!")
         for i, motor in enumerate(self.motors):
             motor.set_state(action[i])
@@ -83,7 +61,7 @@ class Module:
         pass
 
 
-mod = Module((0, 0), ANTI_CLOCKWISE)
+mod = Module((0, 0), ACTIONS["anti_clockwise"])
 print(mod.sr_data)
-mod.set_action(CLOCKWISE)
+mod.set_action(ACTIONS["clockwise"])
 print(mod.sr_data)
--- dev/motor.py
+++ dev/motor.py
@@ -1,18 +1,16 @@
 # valid motor states
-FORWARD = (1, 0)
-BACKWARD = (0, 1)
-RELEASE = (0, 0)
+STATES = {"forward": (1, 0), "backward": (0, 1), "release": (0, 0)}
 
 
 class Motor:
     """Class for defining a motor on the OmniVeyor.
     Manages the motor pins, state, and position on the pi camera."""
 
-    def __init__(self, pins, position, state=RELEASE):
+    def __init__(self, pins, position, state=STATES["release"]):
         """Initialize a motor.
         pins: (forward_pin, backward_pin) on shift register
         position: (x, y) position in pi camera frame
-        state: FORWARD, BACKWARD, or RELEASE (default)"""
+        state: any state from STATES"""
         self.pins = pins
         self.position = position
         self.set_state(state)
@@ -25,7 +23,7 @@ class Motor:
 
     def set_state(self, state):
         """Set motor state.
-        state: FORWARD, BACKWARD, or RELEASE"""
-        if state not in (FORWARD, BACKWARD, RELEASE):
+        state: any state from STATES"""
+        if state not in STATES.values():
             raise Exception("Invalid motor state!")
         self.state = state
