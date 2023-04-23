f909789e99a41ab701e0650dd689c0739197c3e7
Atta ul Haleem <58297355+Attaulhaleem@users.noreply.github.com>
2023-04-23 13:59:59 +0500

    added __str__

--- dev/motor.py
+++ dev/motor.py
@@ -12,14 +12,20 @@ class Motor:
         """Initialize a motor.
         pins: (forward_pin, backward_pin) on shift register
         position: (x, y) position in pi camera frame
-        state: FORWARD, BACKWARD, or RELEASE"""
+        state: FORWARD, BACKWARD, or RELEASE (default)"""
         self.pins = pins
         self.position = position
         self.set_state(state)
 
+    def __str__(self):
+        """Return motor properties as a string."""
+        return "Motor [pins: {}, position: {}, state: {}]".format(
+            self.pins, self.position, self.state
+        )
+
     def set_state(self, state):
         """Set motor state.
         state: FORWARD, BACKWARD, or RELEASE"""
-        if state not in (self.FORWARD, self.BACKWARD, self.RELEASE):
+        if state not in (FORWARD, BACKWARD, RELEASE):
             raise Exception("Invalid motor state!")
         self.state = state
