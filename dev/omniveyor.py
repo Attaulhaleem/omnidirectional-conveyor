b7a9916aadcc0a2b928ad922a8dcb06b705acf47
Atta ul Haleem <58297355+Attaulhaleem@users.noreply.github.com>
2023-04-26 08:57:12 +0500

    added actuate

--- dev/omniveyor.py
+++ dev/omniveyor.py
@@ -25,12 +25,22 @@ class Omniveyor:
         # self.sr.clear()
 
     def update_sr_data(self):
-        """Chain the individual module bytes to write to shift register(s)."""
+        """Chain the individual module bytes to be written to shift register(s)."""
+        chained_data = list()
         for module in self.modules:
-            self.sr_data.append(module.sr_byte)
+            chained_data.append(module.sr_byte)
         # first motor data must be sent last
-        self.sr_data.reverse()
+        chained_data.reverse()
+        # flatten list
+        chained_data = [bit for byte in chained_data for bit in byte]
+        self.sr_data = chained_data
+
+    def actuate(self):
+        """Actuate the Omniveyor motors according to their assigned states."""
+        self.update_sr_data()
+        # self.sr.shift_out(self.sr_data)
 
 
 omni = Omniveyor(10, [(0, 0) for _ in range(10)])
+omni.actuate()
 print(omni)
