e95366959a68921ca0ef4a17bdc31adb84b713e4
Atta ul Haleem <58297355+Attaulhaleem@users.noreply.github.com>
2023-05-05 15:42:09 +0500

    updated

--- dev/app.py
+++ dev/app.py
@@ -17,7 +17,8 @@ class App:
 
     def create_frames(self):
         self.root = Tk()
-        self.root.attributes("-fullscreen", True)
+        # self.root.attributes("-fullscreen", True)
+        self.root.state("zoomed")
         self.root.resizable(0, 0)
         self.root.rowconfigure(0, weight=1)
         self.root.rowconfigure(1, weight=1)
