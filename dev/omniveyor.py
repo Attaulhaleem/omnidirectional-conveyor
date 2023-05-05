7935496022fc5d169b0d525833462f22cd73df36
Atta ul Haleem <58297355+Attaulhaleem@users.noreply.github.com>
2023-05-06 04:25:01 +0500

    added stop

--- dev/omniveyor.py
+++ dev/omniveyor.py
@@ -38,8 +38,17 @@ class Omniveyor:
         # chained_data = [bit for byte in chained_data for bit in byte]
         self.sr_data = chained_data
 
+    def stop(self):
+        """Stop all the motors."""
+        for module in self.modules:
+            module.set_action(ACTIONS["idle"])
+        self.update_sr_data()
+        self.sr1.shift_out(self.sr_data[0:40])
+        self.sr2.shift_out(self.sr_data[40:64])
+
     def actuate(self):
         """Actuate the Omniveyor motors according to their assigned states."""
+        self.stop()
         self.update_module_actions()
         self.update_sr_data()
         print(self.sr_data)
