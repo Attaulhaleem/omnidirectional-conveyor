76a69fab946ea40f889dfd12597750c632f19ce4
Atta ul Haleem <58297355+Attaulhaleem@users.noreply.github.com>
2023-05-05 18:24:07 +0500

    updated

--- dev/shift_register.py
+++ dev/shift_register.py
@@ -60,9 +60,9 @@ if __name__ == "__main__":
         data_list.extend([0, 1, 1, 0, 0, 0, 0, 1])
     sr1.shift_out(data_list)
 
-    sr2 = ShiftRegister(11, 13, 15, 3)
+    sr2 = ShiftRegister(11, 13, 15, 4)
     data_list = []
-    for _ in range(3):
+    for _ in range(4):
         data_list.extend([0, 1, 1, 0, 0, 0, 0, 1])
     sr2.shift_out(data_list)
 
