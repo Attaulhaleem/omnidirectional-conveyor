29a3b544bdd02e7cab414d19ab7433595935e956
Atta ul Haleem <58297355+Attaulhaleem@users.noreply.github.com>
2023-05-09 05:02:28 +0500

    updated

--- dev/detector.py
+++ dev/detector.py
@@ -56,3 +56,37 @@ cv2.namedWindow("image", cv2.WINDOW_NORMAL)
 cv2.imshow("image", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
+def get_contours_image(img):
+    # Gaussian blur
+    blurred = cv2.GaussianBlur(img, (5, 5), 0)
+    # Convert to graysscale
+    gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
+    # Autocalculate the thresholding level
+    threshold = cv2.adaptiveThreshold(
+        gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2
+    )
+    # Threshold
+    retval, bin = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
+    # Find contours
+    contours, hierarchy = cv2.findContours(bin, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    # Sort out the biggest contour (biggest area)
+    max_area = 0
+    max_index = -1
+    index = -1
+    for i in contours:
+        area = cv2.contourArea(i)
+        index = index + 1
+        if area > max_area:
+            max_area = area
+            max_index = index
+    # Draw the raw contours
+    cv2.drawContours(img, contours, max_index, (0, 255, 0), 3)
+    # Draw a rotated rectangle of the minimum area enclosing our box (red)
+    cnt = contours[max_index]
+    rect = cv2.minAreaRect(cnt)
+    box = cv2.boxPoints(rect)
+    box = np.int0(box)
+    img = cv2.drawContours(img, [box], 0, (0, 0, 255), 2)
+    return img
