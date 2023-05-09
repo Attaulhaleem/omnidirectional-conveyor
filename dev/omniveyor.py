4f3d48a42f8469ae27557b3403065f4ff566f4a0
Atta ul Haleem <58297355+Attaulhaleem@users.noreply.github.com>
2023-05-09 07:56:38 +0500

    u

--- dev/omniveyor.py
+++ dev/omniveyor.py
@@ -160,10 +160,8 @@ if __name__ == "__main__":
     act = input("Action: ")
     omni = Omniveyor()
 
-    for i in range(omni.num_of_modules):
-        for module in omni.modules:
-            module.set_action(ACTIONS["idle"])
-
+    for i in range(3):
         omni.modules[i].set_action(ACTIONS.get(act, ACTIONS["idle"]))
-        omni.actuate()
-        time.sleep(3)
+
+    omni.actuate()
+    time.sleep(30)
