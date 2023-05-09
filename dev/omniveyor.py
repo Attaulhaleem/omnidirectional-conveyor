f4483daea10fd5307f4a49d2e828675b1c888e09
Atta ul Haleem <58297355+Attaulhaleem@users.noreply.github.com>
2023-05-09 07:28:39 +0500

    u

--- dev/omniveyor.py
+++ dev/omniveyor.py
@@ -42,7 +42,7 @@ class Omniveyor:
         """Actuate the Omniveyor motors according to their assigned states."""
         self.update_module_actions()
         self.update_sr_data()
-        self.sr1.shift_out(self.sr_data[0:40])
+        self.sr1.shift_out(self.sr_data[0:24])
         self.sr2.shift_out(self.sr_data[40:64])
 
     def update_module_actions(self):
