a185f42374ad1fea35be2a33323656cce88c0e30
Atta ul Haleem <58297355+Attaulhaleem@users.noreply.github.com>
2023-05-16 20:15:07 +0500

    updated

--- dev/test.py
+++
@@ -1,9 +0,0 @@
-from shift_register import ShiftRegister
-
-sr = ShiftRegister(11, 13, 15, 10)
-
-data_list = []
-for _ in range(10):
-    data_list.extend([0, 1, 1, 0, 0, 0, 0, 1])
-
-sr.shift_out(data_list)
